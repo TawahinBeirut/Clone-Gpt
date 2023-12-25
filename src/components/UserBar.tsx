29f3689c6c7a03f0b780e33b3049a19149700bac
tawahinBeirut <gheribij59@gmail.com>
2023-12-25 17:11:42 +0100

    Ajout stylistique sur le composant UserBar

--- src/components/UserBar.tsx
+++ src/components/UserBar.tsx
@@ -1,6 +1,7 @@
 import { signOut } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import { AddApiKeyComp } from "./AddApiKey"
+import Img from "@/../public/urangUtan.png"
 
 type UserBarProps = {
     UserName:string
@@ -12,8 +13,11 @@ const UserBar = ({UserName,UserMail}: UserBarProps) =>{
     const Router = useRouter();
 
     return(
-        <div className="border border-black h-screen p-5 bg-white flex flex-col gap-32">
-            <h1 className="text-xl">Welcolme {UserName}</h1>
+        <div className="border border-black h-screen p-5 bg-white flex flex-col gap-16">
+            <div className="text-center overflow-hidden max-w-44">
+            <img src={Img.src} width="175px"></img>
+            <h1 className="text-xl">{UserName}</h1>
+            </div>
             {/* Profile Image pour la suite */}
             <AddApiKeyComp UserMail={UserMail}/>
             <button onClick={() =>{signOut({callbackUrl:"/registration"})}} className="border border-black text-xl text-red-600 p-5 rounded-xl">Disconnect</button>
